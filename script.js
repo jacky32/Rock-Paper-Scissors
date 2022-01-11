e6673b5215ac9c0897ff00c7653c18a007d54d6f
jacky32 <patrki@atlas.cz>
2022-01-11 20:58:26 +0100

    added UI, end condition

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 let playerScore = 0, computerScore = 0;
-document.addEventListener("click", function game(e){
-    clickedButton = e.target.innerText;
-    
-    switch(playRound(clickedButton,computerPlay())) {
-        case 2:
-            console.log(`Draw! You both played ${clickedButton.toLowerCase()}.`);
-            break;
-        case 1:
-            playerScore++;
-            console.log(`You won! You played ${clickedButton.toLowerCase()} and the computer played ${computerPlay().toLowerCase()}. Current score: ${playerScore}:${computerScore}.`);
-            break;
-        case 0:
-            computerScore++;
-            console.log(`You lost! You played ${clickedButton.toLowerCase()} and the computer played ${computerPlay().toLowerCase()}. Current score: ${playerScore}:${computerScore}.`);
-            break;
-        case 3:
-            console.log("Wrong selection!");
-            break;
-    }
+const score = document.getElementById("score");
+const playerRoll = document.getElementById("playerRoll");
+const computerRoll = document.getElementById("computerRoll");
 
+document.addEventListener("click", function game(e){
+    if(e.target.className == "btn"){
+        clickedButton = e.target.innerText;
+        computerSelection = computerPlay();
+        
+        displayScore(computerSelection);
+        displayRoll(e);
+        if(playerScore >= 5){
+            score.textContent = "YOU WON";
+            document.removeEventListener("click", game);
+        }
+        else if (computerScore >= 5){
+            score.textContent = "COMPUTER WON";
+            document.removeEventListener("click", game);
+        }
+    }
+    
     /* if(playerScore>computerScore) {
         console.log(`Congratulations! You won with a score of ${playerScore}:${computerScore}.`);
     }
@@ -26,6 +27,60 @@ document.addEventListener("click", function game(e){
         console.log(`Unlucky! You lost with a score of ${playerScore}:${computerScore}.`);
     } */
 });
+
+function displayScore(computerSelection){
+    switch(playRound(clickedButton,computerSelection)) {
+        case 2:
+            highlightRoll(2);
+            break;
+        case 1:
+            playerScore++;
+            highlightRoll(1)
+            break;
+        case 0:
+            computerScore++;
+            highlightRoll(0)
+            break;
+        case 3:
+            console.log("Error");
+            break;
+    }
+    score.textContent = playerScore + ":" + computerScore; 
+    blinkScore();
+}
+
+function highlightRoll(roll){
+    if(roll == 1){
+        playerRoll.classList.add("wonRoll");
+        playerRoll.classList.remove("lostRoll", "draw");
+        computerRoll.classList.add("lostRoll");
+        computerRoll.classList.remove("wonRoll", "draw");
+    }
+    else if (roll == 0){
+        playerRoll.classList.add("lostRoll");
+        playerRoll.classList.remove("wonRoll", "draw");
+        computerRoll.classList.add("wonRoll");
+        computerRoll.classList.remove("lostRoll", "draw");
+    }
+    else if (roll == 2) {
+        playerRoll.classList.add("draw");
+        playerRoll.classList.remove("wonRoll", "lostRoll");
+        computerRoll.classList.add("draw");
+        computerRoll.classList.remove("lostRoll", "wonRoll");
+    }
+}
+
+function displayRoll(e){
+    playerRoll.textContent = e.target.innerText;
+    computerRoll.textContent = computerSelection;
+}
+
+function blinkScore(){
+    score.classList.toggle('enhanceScore');
+    setTimeout(function(){
+        score.classList.toggle('enhanceScore');
+    }, 200);
+}
 
 function computerPlay() {
     let vyber = ["Rock", "Paper", "Scissors"];
@@ -35,7 +90,6 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     if(playerSelection.toLowerCase() == "rock" || playerSelection.toLowerCase() == "paper" || playerSelection.toLowerCase() == "scissors") {
         if(playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-            
             return 2;
         }
         else if ((playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase()== "scissors") || (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase()== "paper") || (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase()== "rock"))  {
