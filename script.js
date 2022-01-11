5e7e1ffcd25249c52540507462df393f280624e4
jacky32 <patrki@atlas.cz>
2022-01-11 01:07:56 +0100

    Added basic UI functionality with console output

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+let playerScore = 0, computerScore = 0;
+document.addEventListener("click", function game(e){
+    clickedButton = e.target.innerText;
+    
+    switch(playRound(clickedButton,computerPlay())) {
+        case 2:
+            console.log(`Draw! You both played ${clickedButton.toLowerCase()}.`);
+            break;
+        case 1:
+            playerScore++;
+            console.log(`You won! You played ${clickedButton.toLowerCase()} and the computer played ${computerPlay().toLowerCase()}. Current score: ${playerScore}:${computerScore}.`);
+            break;
+        case 0:
+            computerScore++;
+            console.log(`You lost! You played ${clickedButton.toLowerCase()} and the computer played ${computerPlay().toLowerCase()}. Current score: ${playerScore}:${computerScore}.`);
+            break;
+        case 3:
+            console.log("Wrong selection!");
+            break;
+    }
+
+    /* if(playerScore>computerScore) {
+        console.log(`Congratulations! You won with a score of ${playerScore}:${computerScore}.`);
+    }
+    else {
+        console.log(`Unlucky! You lost with a score of ${playerScore}:${computerScore}.`);
+    } */
+});
+
 function computerPlay() {
     let vyber = ["Rock", "Paper", "Scissors"];
     return(vyber[Math.floor(Math.random()*vyber.length)]);
@@ -17,35 +46,5 @@ function playRound(playerSelection, computerSelection) {
         }
     } else { 
         return 3; 
-    }
-}
-
-function game() {
-    let playerScore = 0, computerScore = 0;
-    while(!((playerScore || computerScore) >= 3)) {
-        let computerSelection = computerPlay();
-        let playerSelection = prompt("Choose:");
-        switch(playRound(playerSelection,computerSelection)) {
-            case 2:
-                console.log(`Draw! You both played ${playerSelection.toLowerCase()}.`);
-                break;
-            case 1:
-                playerScore++;
-                console.log(`You won! You played ${playerSelection.toLowerCase()} and the computer played ${computerSelection.toLowerCase()}. Current score: ${playerScore}:${computerScore}.`);
-                break;
-            case 0:
-                computerScore++;
-                console.log(`You lost! You played ${playerSelection.toLowerCase()} and the computer played ${computerSelection.toLowerCase()}. Current score: ${playerScore}:${computerScore}.`);
-                break;
-            case 3:
-                console.log("Wrong selection!");
-                break;
-        }
-    }
-    if(playerScore>computerScore) {
-        console.log(`Congratulations! You won with a score of ${playerScore}:${computerScore}.`);
-    }
-    else {
-        console.log(`Unlucky! You lost with a score of ${playerScore}:${computerScore}.`);
     }
 }
